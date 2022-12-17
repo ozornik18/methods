f7e76b3f7838ae2920dcd2d5c62da28c7ef701ba
Константин <kuz18rus@ya.ru>
2022-12-17 21:49:59 +0400

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        System.out.println("Задание 1");
+        int currentYear = 2022;
+        if (currentYear % 4 == 0 && currentYear % 100 != 0 || currentYear % 400 == 0) {
+            System.out.println("Год является високосным");
+        } else {
+            System.out.println("год не является високосным");
+        }
     }
 }
